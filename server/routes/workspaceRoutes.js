11bc26569b3fee57ffb086d28107541be6a84b81
Joel-Cornfield <cornfieldjoel@gmail.com>
2026-03-24 11:20:41 +0800

    Quick import fix

--- server/routes/workspaceRoutes.js
+++ server/routes/workspaceRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
-import { createWorkspace, getWorkspaces } from '../controllers/workspaceController';
+import { createWorkspace, getWorkspaces } from '../controllers/workspaceController.js';
 
 const workspaceRoutes = express.Router();
 
