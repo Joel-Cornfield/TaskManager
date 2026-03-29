01f35cad1cfbb8d38e82a948c095586a5312561f
Joel-Cornfield <cornfieldjoel@gmail.com>
2026-03-29 13:16:25 +0800

    add: added delete and update routes for workspaces

--- server/controllers/workspaceController.js
+++ server/controllers/workspaceController.js
@@ -21,4 +21,29 @@ export const createWorkspace = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
+};
+
+export const updateWorkspace = async (req, res, next) => {
+    try {
+        const { name } = req.body;
+        const { id } = req.params;
+        if (!name) {
+            res.status(400);
+            return newError('Name required');
+        }
+        await pool.query('UPDATE workspaces SET name = $1 WHERE id = $2 AND user_id = $3 RETURNING *',[name, id, req.user.id]);
+        res.json(result.rows[0]);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const deleteWorkspace = async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        await pool.query('DELETE FROM workspaces WHERE id = $1 AND user_id = $2', [id, req.user.id]);
+        res.json({ message: 'Workspace deleted' });
+    } catch (error) {
+        next(error)
+    }
 }
--- server/routes/workspaceRoutes.js
+++ server/routes/workspaceRoutes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
-import { createWorkspace, getWorkspaces } from '../controllers/workspaceController.js';
+import { createWorkspace, deleteWorkspace, getWorkspaces, updateWorkspace } from '../controllers/workspaceController.js';
 
 const workspaceRoutes = express.Router();
 
 workspaceRoutes.get('/', protect, getWorkspaces);
 workspaceRoutes.post('/', protect, createWorkspace);
+workspaceRoutes.put('/', protect, updateWorkspace);
+workspaceRoutes.delete('/', protect, deleteWorkspace);
 
 export default workspaceRoutes;
