8c280bd5129f32de73b5e803121d21ed714a557d
Joel-Cornfield <cornfieldjoel@gmail.com>
2026-03-29 12:28:22 +0800

    Fixed incorrect prop usage

--- client/src/components/WorkspaceList.jsx
+++ client/src/components/WorkspaceList.jsx
@@ -3,14 +3,14 @@ import WorkspaceItem from './WorkspaceItem';
 
 const WorkspaceList = ({ workspaces } ) => {
   return (
-    <div className="column">
+    <div className="workspace-list">
         <h2>Your Workspaces</h2>
         {workspaces.length === 0 && <p>No workspaces yet...</p>}
-        {workspaces?.map(workspace => (
-            <WorkspaceItem key={workspace.id} task={workspace}/>
+        {workspaces?.map((workspace) => (
+            <WorkspaceItem key={workspace.id} workspace={workspace}/>
         ))}
     </div>
-  )
-}
+  );
+};
 
 export default WorkspaceList;
