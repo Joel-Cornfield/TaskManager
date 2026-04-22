cdffdc9ade0c955ca78ceeeec5b596d98cbb8093
Joel-Cornfield <cornfieldjoel@gmail.com>
2026-04-22 10:31:54 +0800

    feat: ensure uploads directory is accessible in browser

--- server/server.js
+++ server/server.js
@@ -16,9 +16,10 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ['http://localhost:5173'], // your frontend
+  origin: ['http://localhost:5173'],
   credentials: true
 }));
+app.use('/uploads', express.static('uploads')); // /uploads accessible in browser
 
 // Routes
 app.get('/', (req, res) => {
